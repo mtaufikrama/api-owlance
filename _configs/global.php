75f21613caddddf104d5bedec541a00f61b8347e
mtaufikrama <mtaufikrama14@gmail.com>
2023-09-25 18:08:37 +0700

    global api

--- _configs/global.php
+++ _configs/global.php
@@ -7,11 +7,11 @@ $AV_CONF["db"]["pass"]="$534UlWhP";
 $AV_CONF["db"]["name"]="zyarnfpx_db_owlance";
 $AV_CONF["db"]["host"]="127.0.0.1";
 
-$AV_CONF["db"]["type"]="mysqli";
-$AV_CONF["db"]["user"]="root";
-$AV_CONF["db"]["pass"]="";
-$AV_CONF["db"]["name"]="a-dokter";
-$AV_CONF["db"]["host"]="127.0.0.1";
+// $AV_CONF["db"]["type"]="mysqli";
+// $AV_CONF["db"]["user"]="root";
+// $AV_CONF["db"]["pass"]="";
+// $AV_CONF["db"]["name"]="owlance";
+// $AV_CONF["db"]["host"]="127.0.0.1";
 
 // $AV_CONF["db"]["type"]="mssqlnative";
 // $AV_CONF["db"]["user"]="sa";
