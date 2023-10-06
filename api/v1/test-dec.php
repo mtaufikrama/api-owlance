31a13738403fef811626aa99b852ed72bdaf70f3
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-06 16:50:22 +0700

    test decrypt

--- api/v1/test-dec.php
+++ api/v1/test-dec.php
@@ -2,11 +2,6 @@
 
 include '../encrypt.php';
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-$dataSend = file_get_contents("php://input");
-$json = dekrip($dataSend);
-$enkrip = base64_decode($json);
-$dekrip = json_decode($enkrip, true);
-$echo = json_encode($enkrip);
-echo $echo;
+echo decryptData();
