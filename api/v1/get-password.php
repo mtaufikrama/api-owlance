31909f850f69d9a8178c064930fa78f0fed3e806
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-06 16:56:05 +0700

    test decrypt

--- api/v1/get-password.php
+++ api/v1/get-password.php
@@ -7,4 +7,4 @@ $password = randomString();
 $data['code'] = 200;
 $data['msg'] = $password;
 
-echo json_encode($data);
+echo encryptData($data);
--- api/v1/test-dec.php
+++ api/v1/test-dec.php
@@ -1,7 +1,11 @@
 <?php
 
-// include '../encrypt.php';
+include '../encrypt.php';
 
 header('Content-Type: application/json');
 
-echo json_encode(decryptData());
+$dataSend = file_get_contents("php://input");
+$json = dekrip($dataSend);
+$enkrip = base64_decode($json);
+$dekrip = json_decode($enkrip, true);
+echo json_encode($dekrip);
