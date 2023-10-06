18c280c4300d7297e0dd1dd0a63cc5b8e70078ff
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-06 16:30:06 +0700

    cek email api

--- api/v1/cek-email.php
+++ api/v1/cek-email.php
@@ -1,6 +1,6 @@
 <?php
 
-include "cek-token.php";
+include "../src/export.php";
 
 //email
 $cek = baca_tabel("user", "count(email)", "where email='$email'");
@@ -9,12 +9,10 @@ if ($cek > 0) {
 
     $data['code'] = 500;
     $data['msg'] = "Email Sudah Digunakan!";
-
 } else {
 
     $data['code'] = 200;
     $data['msg'] = "Email tidak Terdaftar";
-
 }
 
 echo encryptData($data);
