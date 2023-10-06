65242a3fbb54b26916b1a218f008939a112793aa
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-06 16:47:42 +0700

    test decrypt

--- api/v1/test-dec.php
+++ api/v1/test-dec.php
@@ -8,5 +8,5 @@ $dataSend = file_get_contents("php://input");
 $json = dekrip($dataSend);
 $enkrip = base64_decode($json);
 $dekrip = json_decode($enkrip, true);
-$echo = json_decode($enkrip, true);
+$echo = json_encode($enkrip);
 echo $echo;
