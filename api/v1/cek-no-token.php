b688b008233fb2a0a7d93870661374451b5926c6
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-09 01:25:21 +0700

    cek no token

--- api/v1/cek-no-token.php
+++ api/v1/cek-no-token.php
@@ -2,16 +2,16 @@
 include "../src/export.php";
 // $db->debug=true;
 
-$dataSend = json_decode(file_get_contents("php://input"), true);
-foreach ($dataSend as $key => $val) {
-	$$key = $val;
-}
+// $dataSend = json_decode(file_get_contents("php://input"), true);
+// foreach ($dataSend as $key => $val) {
+// 	$$key = $val;
+// }
 foreach ($_FILES as $key => $val) {
 	$$key = $val;
 }
-foreach ($_GET as $key => $val) {
-	$$key = $val;
-}
+// foreach ($_GET as $key => $val) {
+// 	$$key = $val;
+// }
 foreach ($_POST as $key => $val) {
 	$$key = $val;
 }
