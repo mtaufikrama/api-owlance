e00a6021ceda75855bd054d81957dba5c9df80f3
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-09 04:06:16 +0700

    register api

--- api/v1/cek-username.php
+++ api/v1/cek-username.php
@@ -3,15 +3,20 @@
 include 'cek-no-token.php';
 
 //username
+if ($username) {
 
-$cek = baca_tabel("user", "count(*)", "where username='$username'");
-
-if ($cek > 0) {
+    $cek = baca_tabel("user", "count(*)", "where username='$username'");
+    
+    if ($cek > 0) {
+        $datarest['code'] = 500;
+        $datarest['msg'] = "Username Sudah Terdaftar";
+    } else {
+        $datarest['code'] = 200;
+        $datarest['msg'] = "Username Bisa Digunakan";
+    }
+} else {
     $datarest['code'] = 500;
     $datarest['msg'] = "Username Sudah Terdaftar";
-} else {
-    $datarest['code'] = 200;
-    $datarest['msg'] = "Username Bisa Digunakan";
 }
 
 echo encryptData($datarest);
