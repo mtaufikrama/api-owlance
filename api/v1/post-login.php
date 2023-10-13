55053c28c83ed4940e2356a3e4fc5cfcc63dfae4
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-13 17:30:23 +0700

    login api

--- api/v1/post-login.php
+++ api/v1/post-login.php
@@ -11,13 +11,13 @@ $cekemail = baca_tabel('user', 'count(*)', "where email = '$username' or no_hp =
 if ($cekemail > 0) {
     $cek = baca_tabel('user', "count(*)", "where (email = '$username' or no_hp = '$username' or username = '$username') and password = '$pass'");
     if ($cek > 0) {
-        // $json_device = json_encode($device_data);
+        $json_device = json_encode($device_data);
         $id_user = baca_tabel('user', 'id', "where (email = '$username' or no_hp = '$username' or username = '$username') and password = '$pass'");
-        $token = baca_tabel('login', 'token', "where id_user = '$id_user' and device_data = '$device_data'");
+        $token = baca_tabel('login', 'token', "where id_user = '$id_user' and device_data = '$json_device'");
         if (!$token) {
             $data['id_user'] = $id_user;
             $data['token'] = generateID(50, 'login', 'token');
-            $data['device_data'] = $device_data;
+            $data['device_data'] = $json_device;
             $data['waktu'] = date("Y-m-d H:i:s");
             $insert = insert_tabel('login', $data);
             if ($insert) {
@@ -30,7 +30,7 @@ if ($cekemail > 0) {
             }
         } else {
             $data['waktu'] = date("Y-m-d H:i:s");
-            $update = update_tabel('login', $data, "where id_user = '$id_user' and device_data = '$device_data'");
+            $update = update_tabel('login', $data, "where id_user = '$id_user' and device_data = '$json_device'");
             if ($update) {
                 $dataRes['code'] = 200;
                 $dataRes['msg'] = 'Berhasil Login';
