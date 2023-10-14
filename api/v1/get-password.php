492bc8fd7b72d497435e5a58424f0c3f7e0a071d
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-14 11:01:25 +0700

    get pass api

--- api/v1/get-password.php
+++ api/v1/get-password.php
@@ -2,16 +2,9 @@
 
 include 'cek-no-token.php';
 
-$length = 10;
-$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*';
-$randomString = '';
-$charLength = strlen($characters);
-
-for ($i = 0; $i < $length; $i++) {
-    $randomString .= $characters[rand(0, $charLength - 1)];
-}
+$password = randomString();
 
 $data['code'] = 200;
-$data['msg'] = $randomString;
+$data['msg'] = $password;
 
 echo encryptData($data);
