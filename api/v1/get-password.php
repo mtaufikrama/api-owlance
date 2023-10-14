297ce16db3b312b1e2736c44fd1326bda380c106
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-14 10:52:50 +0700

    username dan roles

--- api/v1/get-password.php
+++ api/v1/get-password.php
@@ -2,9 +2,16 @@
 
 include 'cek-no-token.php';
 
-$password = randomString();
+$length = 10;
+$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*';
+$randomString = '';
+$charLength = strlen($characters);
+
+for ($i = 0; $i < $length; $i++) {
+    $randomString .= $characters[rand(0, $charLength - 1)];
+}
 
 $data['code'] = 200;
-$data['msg'] = $password;
+$data['msg'] = $randomString;
 
 echo encryptData($data);
