08c79b995e77ee4f4e5aa74252118ead8391cd20
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-17 10:33:02 +0700

    tambah content type

--- api/src/export.php
+++ api/src/export.php
@@ -18,22 +18,23 @@ include "../../_lib/function/function.like_text.php";
 // include "../../_lib/function/variabel.php";
 // header('Content-Type: application/json; charset=utf-8');
 date_default_timezone_set('Asia/Jakarta');
+header("Content-Type: text/plain; charset=utf-8");
 
-// Izinkan akses dari domain Anda
-header("Access-Control-Allow-Origin: https://owlance.metir.my.id");
+// // Izinkan akses dari domain Anda
+// header("Access-Control-Allow-Origin: https://owlance.metir.my.id");
 
-// Izinkan metode HTTP yang diperlukan
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+// // Izinkan metode HTTP yang diperlukan
+// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-// Izinkan header yang diperlukan dalam permintaan
-header("Access-Control-Allow-Headers: Content-Type");
+// // Izinkan header yang diperlukan dalam permintaan
+// header("Access-Control-Allow-Headers: Content-Type");
 
-// Izinkan pengiriman kredensial (jika diperlukan)
-header("Access-Control-Allow-Credentials: true");
+// // Izinkan pengiriman kredensial (jika diperlukan)
+// header("Access-Control-Allow-Credentials: true");
 
-// Perlakukan permintaan OPTIONS sebagai permintaan pra-penerbangan dan atur waktu kadaluwarsa cache
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Max-Age: 1728000");
-    header("Content-Length: 0");
-    header("Content-Type: text/plain");
-}
+// // Perlakukan permintaan OPTIONS sebagai permintaan pra-penerbangan dan atur waktu kadaluwarsa cache
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//     header("Access-Control-Max-Age: 1728000");
+//     header("Content-Length: 0");
+//     header("Content-Type: text/plain");
+// }
