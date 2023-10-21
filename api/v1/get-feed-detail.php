f67aba2aaacb7cc7b4912447a24bdac5a79bf412
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-21 21:12:53 +0700

    feed api

--- api/v1/get-feed-detail.php
+++ api/v1/get-feed-detail.php
@@ -1,18 +1,28 @@
 <?php
 include "cek-no-token.php";
 
-// nama, id
+// id
 
-switch ($nama) {
-	case "feed":
-		$sql = "SELECT a.id, a.judul, a.caption, a.waktu, 
-		b.nama, b.username, b.foto, 
+$id_tabs = baca_tabel("feed", "id_tabs", "where id='$id'");
+
+if (!$id_tabs || $id_tabs == '') {
+	$datax['code'] = 404;
+	$datax['msg'] = "Data Tidak Ditemukan";
+	echo encryptData($datax);
+	die();
+}
+
+switch ($id_tabs) {
+	case "1b2IDNZbMY5JJ0e":
+		$sql = "SELECT a.id, e.nama, a.judul, a.caption, a.waktu, 
+		b.nama as nama_pengguna, b.foto, 
 		COUNT(c.*) as jml_like, 
 		COUNT(d.*) as jml_comment
 		FROM feed a 
 		JOIN user b ON a.id_user=b.id 
 		JOIN like c ON a.id=c.id_feed 
 		JOIN comment d ON a.id=d.id_feed
+		JOIN tabs e ON a.id_tabs=e.id
 		WHERE a.id='$id'";
 
 		$run = $db->Execute($sql);
@@ -50,7 +60,7 @@ switch ($nama) {
 			$data = $get;
 		}
 		break;
-	case "gig":
+	case "gxGAOusmXfQRi51":
 		$sql = "SELECT a.id, a.judul, a.caption, a.waktu, 
 		b.nama, b.username, b.foto, 
 		COUNT(c.*) as jml_like, 
@@ -75,19 +85,44 @@ switch ($nama) {
 			$data = $get;
 		}
 		break;
-	case "project":
+	case "odlScUrxIfVq2y7":
+		$sql = "SELECT a.id, a.judul, a.caption, a.waktu, 
+		b.nama, b.username, b.foto, 
+		COUNT(c.*) as jml_like, 
+		COUNT(d.*) as jml_comment
+		FROM gigs a 
+		JOIN user b ON a.id_user=b.id 
+		JOIN like c ON a.id=c.id_feed 
+		JOIN comment d ON a.id=d.id_feed
+		WHERE a.id='$id'";
+
+		$run = $db->Execute($sql);
+
+		while ($get = $run->fetchRow()) {
+			$sqlImg = "SELECT foto FROM gigs_img WHERE id_gigs='$id'";
+
+			$runImg = $db->Execute($sqlImg);
+
+			while ($getImg = $runImg->fetchRow()) {
+				$images[] = $getImg;
+				$data['images'] = $images;
+			}
+			$data = $get;
+		}
 		break;
 
 	default:
-
-		break;
+		$datax['code'] = 404;
+		$datax['msg'] = "Nama dan ID kosong";
+		echo encryptData($datax);
+		die;
 }
 
-if ($nama || $id) {
+if (is_array($data)) {
 	$datax['code'] = 200;
 	$datax['data'] = $data;
 } else {
 	$datax['code'] = 500;
-	$datax['msg'] = "Nama dan ID kosong";
+	$datax['msg'] = "Data Tidak Ditemukan";
 }
 echo encryptData($datax);
