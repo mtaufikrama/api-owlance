b244021afd2fa8c6fda942e17d164b0f06f86c28
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-23 11:25:22 +0700

    list comment

--- api/v1/list-comment.php
+++ api/v1/list-comment.php
@@ -1,27 +1,23 @@
 <?php
 include "cek-token.php";
 
-// nama, id
-switch ($nama) {
-	case 'feed':
-		$sql = "SELECT a.comment, a.rate, b.username, b.foto 
-		from comment a 
-		join user b on a.id_user=b.id 
-		where id_feed='$id' order by a.waktu desc";
-		break;
-	case 'gig':
-		$sql = "SELECT a.comment, a.rate, b.username, b.foto 
-		from comment a 
-		join user b on a.id_user=b.id 
-		where id_gigs='$id' order by a.waktu desc";
-		break;
-	case 'project':
-		$sql = "SELECT a.comment, a.rate, b.username, b.foto 
-		from comment a 
-		join user b on a.id_user=b.id 
-		where id_gigs='$id' order by a.waktu desc";
-		break;
+// tabs, id
+
+$cek = baca_tabel('tabs', 'count(*)', "where nama='$tabs'");
+
+if ($cek <= 0) {
+	$datax['code'] = 404;
+	$datax['msg'] = "Tidak ada data ditemukan";
+	echo encryptData($datax);
+	die();
 }
+
+$id_tabs = baca_tabel('tabs', 'id', "where nama='$tabs'");
+
+$sql = "SELECT a.comment, a.rate, b.username, b.foto 
+	from comment a 
+	join user b on a.id_user=b.id 
+	where id_tabs='$id_tabs' and kode='$id' order by a.waktu desc";
 
 $run = $db->Execute($sql);
 
@@ -31,7 +27,7 @@ while ($get = $run->fetchRow()) {
 
 if (is_array($data)) {
 	$datax['code'] = 200;
-	$datax['lists'] = $data;
+	$datax['comment'] = $data;
 } else {
 	$datax['code'] = 500;
 	$datax['msg'] = "Tidak ada data ditemukan";
