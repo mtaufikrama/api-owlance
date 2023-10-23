ee15cd3afcd4236a900f0137526de324b814a931
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-23 10:18:36 +0700

    cek detail api

--- api/v1/cek-detail.php
+++ api/v1/cek-detail.php
@@ -3,6 +3,15 @@ include "cek-no-token.php";
 
 // id, tabs
 
+$cek = baca_tabel('tabs', 'count(*)', "where nama='$tabs'");
+
+if ($cek <= 0) {
+	$datax['code'] = 404;
+	$datax['msg'] = "Data Tidak Ditemukan";
+	echo encryptData($datax);
+	die();
+}
+
 $cek = baca_tabel($tabs, 'count(*)', "where id='$id'");
 
 if ($cek <= 0) {
