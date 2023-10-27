f23b0386752a5e56e49058107b177cae728464a6
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-27 18:14:40 +0700

    list login dan get profile api

--- api/v1/post-sip.php
+++ api/v1/post-sip.php
@@ -13,6 +13,13 @@ $size = 0;
 
 foreach ($_FILES as $key => $element) {
     $size = $size + $element['size'];
+    if ($size >= 2000000) {
+        $datax['code'] = 500;
+        $datax['msg'] = 'Data Terlalu Besar';
+        echo json_encode($datax);
+        die();
+    }
+    $element['image'] = ("data:" . $get['type'] . ";base64," . base64_encode(file_get_contents($element['tmp_name'])));
     $get[] = $element;
 }
 $post['size'] = $size;
