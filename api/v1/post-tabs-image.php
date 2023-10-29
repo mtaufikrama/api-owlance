9a1760670465d05117e6100ac595e5c9bb50c6a5
mtaufikrama <mtaufikrama14@gmail.com>
2023-10-29 21:12:23 +0700

    tabs image

--- api/v1/post-tabs-image.php
+++ api/v1/post-tabs-image.php
@@ -1,6 +1,17 @@
 <?php
 
-include "cek-no-token.php";
+include "cek-token.php";
+
+//array(files), tabs, kode
+
+$id_tabs = baca_tabel("tabs", "id", "where nama='$tabs'");
+
+if (!$id_tabs || $id_tabs == '') {
+    $datax['code'] = 404;
+    $datax['msg'] = "Data Tidak Ditemukan";
+    echo encryptData($datax);
+    die();
+}
 
 if (!is_array($_FILES)) {
     $datax['code'] = 500;
@@ -19,17 +30,17 @@ foreach ($_FILES as $key => $element) {
         echo json_encode($datax);
         die();
     }
-    $images[] = ("data:" . $element['type'] . ";base64," . base64_encode(file_get_contents($element['tmp_name'])));
+    $images[] = $element;
 }
 
-unset($element);
-
 foreach ($images as $image) {
-    $feedImg['id'] = generateID(15, 'feed_img', 'id');
-    $feedImg['id_feed'] = $id;
-    $feedImg['image'] = $image;
-    // $result = insert_tabel('feed_img', $feedImg);
-    $feed[] = $feedImg;
+    $userImg['id'] = generateID(50, 'tabs_img', 'id');
+    $userImg['id_tabs'] = $id_tabs;
+    $userImg['kode'] = $kode;
+    $userImg['mime_type'] = $image['type'];
+    $userImg['image'] = base64_encode(file_get_contents($image['tmp_name']));
+    $userImg['waktu'] = date_time();
+    $result = insert_tabel('tabs_img', $userImg);
 }
 
 echo json_encode($feed);
